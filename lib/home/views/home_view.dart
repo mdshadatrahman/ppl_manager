9352f300e680c3eec2d050361f72b3ea245e4660
Shadat Rahman <shadat.rahman.464@gmail.com>
2024-02-17 03:51:22 +0600

    custom paint

--- lib/home/views/home_view.dart
+++ lib/home/views/home_view.dart
@@ -7,6 +7,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:people_manager/gen/assets.gen.dart';
 import 'package:people_manager/home/bloc/home_bloc.dart';
 import 'package:people_manager/home/views/create_and_edit_view.dart';
+import 'package:people_manager/home/widgets/custom_paints.dart';
 import 'package:people_manager/home/widgets/tab_button.dart';
 import 'package:people_manager/home/widgets/user_card.dart';
 import 'package:people_manager/utils/app_colors.dart';
@@ -50,131 +51,152 @@ class _HomeViewState extends State<HomeView> {
         ),
         child: SvgPicture.asset(Assets.svgs.addIcon),
       ),
-      body: SafeArea(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: AppSizes.horizontalPadding.w,
-            ),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  children: [
-                    Expanded(
-                      child: TabButton(
-                        isActive: isActivatedTabOpen,
-                        onTap: () {
-                          setState(() {
-                            isActivatedTabOpen = true;
-                          });
-                        },
-                        text: AppStrings.active,
-                      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              ClipPath(
+                clipper: CustomShape(),
+                child: Container(
+                  height: 180.h,
+                  color: AppColors.primaryColor,
+                ),
+              ),
+              CustomPaint(
+                painter: CustomShapeBorder(),
+                child: Container(
+                  height: 180.h,
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: AppSizes.horizontalPadding.w,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    // SizedBox(height: 25.h),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TabButton(
+                            isActive: isActivatedTabOpen,
+                            onTap: () {
+                              setState(() {
+                                isActivatedTabOpen = true;
+                              });
+                            },
+                            text: AppStrings.active,
+                          ),
+                        ),
+                        SizedBox(width: 16.w),
+                        Expanded(
+                          child: TabButton(
+                            isActive: !isActivatedTabOpen,
+                            onTap: () {
+                              setState(() {
+                                isActivatedTabOpen = false;
+                              });
+                            },
+                            text: AppStrings.deactivate,
+                          ),
+                        ),
+                      ],
                     ),
-                    SizedBox(width: 16.w),
-                    Expanded(
-                      child: TabButton(
-                        isActive: !isActivatedTabOpen,
-                        onTap: () {
-                          setState(() {
-                            isActivatedTabOpen = false;
-                          });
-                        },
-                        text: AppStrings.deactivate,
-                      ),
+                    // SizedBox(height: 16.h),
+                    BlocConsumer<HomeBloc, HomeState>(
+                      bloc: context.read<HomeBloc>(),
+                      listener: (context, state) {},
+                      builder: (context, state) {
+                        if (state is HomeInitialState || state is HomeLoadingState) {
+                          return const Center(
+                            child: CircularProgressIndicator.adaptive(
+                              backgroundColor: AppColors.primaryColor,
+                            ),
+                          );
+                        }
+
+                        if (state is HomeErrorState) {
+                          return Center(
+                            child: Text(
+                              state.error,
+                              style: TextStyle(
+                                color: AppColors.red,
+                                fontSize: 16.sp,
+                              ),
+                            ),
+                          );
+                        }
+
+                        if (state is HomeLoadedState) {
+                          if (isActivatedTabOpen && context.read<HomeBloc>().activeUsers.isEmpty) {
+                            return Center(
+                              child: Text(
+                                AppStrings.noActivePeopleFound,
+                                style: TextStyle(
+                                  color: AppColors.lightText,
+                                  fontSize: 16.sp,
+                                ),
+                              ),
+                            );
+                          }
+
+                          if (!isActivatedTabOpen && context.read<HomeBloc>().inactiveUsers.isEmpty) {
+                            return Center(
+                              child: Text(
+                                AppStrings.noInactivePeopleFound,
+                                style: TextStyle(
+                                  color: AppColors.lightText,
+                                  fontSize: 16.sp,
+                                ),
+                              ),
+                            );
+                          }
+                          return Expanded(
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: isActivatedTabOpen ? context.read<HomeBloc>().activeUsers.length : context.read<HomeBloc>().inactiveUsers.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                final user = isActivatedTabOpen ? context.read<HomeBloc>().activeUsers[index] : context.read<HomeBloc>().inactiveUsers[index];
+                                return Padding(
+                                  padding: EdgeInsets.only(bottom: 8.h),
+                                  child: UserCard(
+                                    name: user.name ?? '',
+                                    email: user.email ?? '',
+                                    gender: user.gender ?? '',
+                                    profilePlaceholderColor: getColor(index),
+                                    isDeactivated: user.status == ActiveStatus.inactive.name,
+                                    onTap: () {
+                                      Navigator.of(context).push(
+                                        MaterialPageRoute<void>(
+                                          builder: (BuildContext context) {
+                                            return CreateAndEditView(
+                                              user: user,
+                                            );
+                                          },
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                );
+                              },
+                            ),
+                          );
+                        }
+                        return const SizedBox();
+                      },
                     ),
                   ],
                 ),
-                SizedBox(height: 16.h),
-                BlocConsumer<HomeBloc, HomeState>(
-                  bloc: context.read<HomeBloc>(),
-                  listener: (context, state) {},
-                  builder: (context, state) {
-                    if (state is HomeInitialState || state is HomeLoadingState) {
-                      return const Center(
-                        child: CircularProgressIndicator.adaptive(
-                          backgroundColor: AppColors.primaryColor,
-                        ),
-                      );
-                    }
-
-                    if (state is HomeErrorState) {
-                      return Center(
-                        child: Text(
-                          state.error,
-                          style: TextStyle(
-                            color: AppColors.red,
-                            fontSize: 16.sp,
-                          ),
-                        ),
-                      );
-                    }
-
-                    if (state is HomeLoadedState) {
-                      if (isActivatedTabOpen && context.read<HomeBloc>().activeUsers.isEmpty) {
-                        return Center(
-                          child: Text(
-                            AppStrings.noActivePeopleFound,
-                            style: TextStyle(
-                              color: AppColors.lightText,
-                              fontSize: 16.sp,
-                            ),
-                          ),
-                        );
-                      }
-
-                      if (!isActivatedTabOpen && context.read<HomeBloc>().inactiveUsers.isEmpty) {
-                        return Center(
-                          child: Text(
-                            AppStrings.noInactivePeopleFound,
-                            style: TextStyle(
-                              color: AppColors.lightText,
-                              fontSize: 16.sp,
-                            ),
-                          ),
-                        );
-                      }
-
-                      return Expanded(
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: isActivatedTabOpen ? context.read<HomeBloc>().activeUsers.length : context.read<HomeBloc>().inactiveUsers.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            final user = isActivatedTabOpen ? context.read<HomeBloc>().activeUsers[index] : context.read<HomeBloc>().inactiveUsers[index];
-                            return Padding(
-                              padding: EdgeInsets.only(bottom: 8.h),
-                              child: UserCard(
-                                name: user.name ?? '',
-                                email: user.email ?? '',
-                                gender: user.gender ?? '',
-                                profilePlaceholderColor: getColor(index),
-                                isDeactivated: user.status == ActiveStatus.inactive.name,
-                                onTap: () {
-                                  Navigator.of(context).push(
-                                    MaterialPageRoute<void>(
-                                      builder: (BuildContext context) {
-                                        return CreateAndEditView(
-                                          user: user,
-                                        );
-                                      },
-                                    ),
-                                  );
-                                },
-                              ),
-                            );
-                          },
-                        ),
-                      );
-                    }
-                    return const SizedBox();
-                  },
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
---
+++ lib/home/widgets/custom_paints.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CustomShape extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final height = size.height;
+    final width = size.width;
+    final path = Path()
+      ..lineTo(0, height - 100)
+      ..quadraticBezierTo(width / 2, height - 10, width, height - 100)
+      ..lineTo(width, 0)
+      ..close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
+
+class CustomShapeBorder extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2;
+
+    final path = Path()
+      ..moveTo(1, 0) // 1 pixel to the right
+      ..lineTo(size.width, 0)
+      ..lineTo(size.width, size.height - 100)
+      ..moveTo(size.height - 100, size.width)
+      ..moveTo(size.width, size.height - 100)
+      ..close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
